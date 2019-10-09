82e18052838fbba4465f00e72c43e59ab56a9a40
John Skiles Skinner <jno.skinner@gmail.com>
2019-10-09 19:30:40 -0400

    Write ISBN lookup API call

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,21 @@
 class ApiController < ApplicationController
 
   def isbns
-    render plain: "you found it"
+    # Look up ISBNs from params
+    isbns = Isbn.where(code: params[:isbns])
+    # Get the books represented by ISBNs
+    books = isbns.map{|isbn| isbn.book}
+    # Get the syllabi that assign those books
+    syllabi = books.map{|book| book.syllabi}.flatten
+    # Get all books assigned by those syllabi
+    reading_list = syllabi.map{|s| s.books}.flatten
+    # Subtract books the user queried with
+    new_books = reading_list - books
+    # Find ISBN of the resultant books, grouped by book
+    new_isbns = new_books.map{|book| book.isbns.map(&:code)}.uniq
+
+    render plain: new_isbns
+
   end
 
 end
