3d7273c2926de0e23ccf1715714a6438cbc7f8e6
John Skiles Skinner <jno.skinner@gmail.com>
2019-10-29 16:58:18 -0400

    Add views of CIP fields and books in those fields

--- app/controllers/reading_lists_controller.rb
+++ app/controllers/reading_lists_controller.rb
@@ -10,5 +10,18 @@ class ReadingListsController < ApplicationController
     @syllabi = @book.syllabi
     @coassigned = @syllabi.map{|s| s.books}.flatten.uniq - [@book]
   end
-    
+
+  def cips
+    @cips = Syllabus.distinct.pluck(:field, :cip).reject{|e| e[0].nil?}
+  end
+
+  def field
+    # Head the page with a syllabus field label
+    @h1 = params[:field]
+    # Get the ids of all syllabi with this CIP code
+    syllabus_ids = Syllabus.where(cip: params[:cip]).pluck(:id)
+    # Then get all the books associated with those syllabus ids
+    @books = Book.includes(:syllabi).where(syllabi: {id: syllabus_ids}).page params[:page]
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
   resources :isbns
   resources :books
 
+  # API routes
   get 'api/coassigned', controller: 'api', action: 'coassigned'
   get 'api/cips', controller: 'api', action: 'cips'
 
+  # Custom views
   get '/reading_lists', controller: 'reading_lists', action: 'index'
   get 'reading_lists/coassigned', controller: 'reading_lists', action: 'coassigned'
+  get 'reading_lists/cips', controller: 'reading_lists', action: 'cips'
+  get 'reading_lists/field', controller: 'reading_lists', action: 'field'
 
   # https://github.com/plataformatec/devise/wiki/How-To:-Set-up-devise-as-a-single-user-system
   devise_for :users, controllers: { registrations: "registrations"}
